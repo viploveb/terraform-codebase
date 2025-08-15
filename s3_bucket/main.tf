d3e791a002cdb26c1b1a5ef7e2a206ec103f314d
Viplove Bansal <viplove@Viploves-MacBook-Air.local>
2025-08-15 23:43:51 +0530

    enabled versioning and sse

--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -6,7 +6,7 @@ resource "random_id" "this" {
   byte_length = 8
 }
 
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "terraform_backend_bucket" {
   bucket = "terraform-state-${random_id.this.hex}"
   region = "us-east-1"
   tags = {
@@ -14,8 +14,25 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "terraform_backend_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_backend_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_backend_bucket_encryption" {
+  bucket = aws_s3_bucket.terraform_backend_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 
 output "bucket_id" {
   description = "terraform state s3 bucket id"
-  value = resource.aws_s3_bucket.this.id
+  value       = resource.aws_s3_bucket.terraform_backend_bucket.id
 }
